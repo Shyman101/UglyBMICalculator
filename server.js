5471571d68851f43af919865d86d0578f8ba761a
shyman <shyman.o10@gmail.com>
2020-10-28 00:18:37 +0530

    add server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", function(req, res){
+    res.send("<h1>Welcome to BMI Calculator.</h1>");
+});
+
+app.get("/bmiCalculator", function(req, res){
+    res.sendFile(__dirname + "/BMIcalculator.html");
+});
+
+app.post("/bmicalculator", function(req, res){
+    var height = Number(req.body.height);
+    var weight = Number(req.body.weight);
+    var bmi = weight/ (height*height);
+    res.send("Your BMI is " + bmi);
+
+});
+
+app.listen("3000", function(){
+    console.log("The server has started!!!");
+});
